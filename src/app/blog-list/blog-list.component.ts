a4b4eb908c0cf27ba77cd1109e26116628567ba7
vidhurraj147 <vidhurraj147@gmail.com>
2019-01-10 11:19:49 -0500

    ng generate pipe truncate __ truncate pipe is created

--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -9,13 +9,18 @@ import { BlogPost } from '../blog-post';
 export class BlogListComponent implements OnInit {
 
   blogPost: BlogPost[] = [];
-  constructor() { }
+  constructor() { 
+    
+  }
 
+  // Best practice is to leave constructor very light weight
+  // constructor is recomended use for dependency injection for services etc.,
+  // The more compilicated your contructor is MORE difficult to test the class / component
   ngOnInit() {
     let blog1 = new BlogPost('Blog Post 1','Summary1');
     this.blogPost.push(blog1);
     this.blogPost.push(new BlogPost('Blog Post 2','Summary2'));
-    this.blogPost.push(new BlogPost('Blog Post 3','Summary3'));
+    this.blogPost.push(new BlogPost('Blog Post 3','The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component'));
   }
 
 }
