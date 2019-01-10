b9914e74d3c4299dc28d28d6b41e5f60646a9f7f
vidhurraj147 <vidhurraj147@gmail.com>
2019-01-10 12:14:02 -0500

    Implemented pages blog posts for each blog

--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -8,7 +8,8 @@ import { BlogPost } from '../blog-post';
 })
 export class BlogListComponent implements OnInit {
 
-  blogPost: BlogPost[] = [];
+  // blogPost: BlogPost[] = [];
+  blogPosts: BlogPost[][];
   constructor() { 
     
   }
@@ -17,10 +18,46 @@ export class BlogListComponent implements OnInit {
   // constructor is recomended use for dependency injection for services etc.,
   // The more compilicated your contructor is MORE difficult to test the class / component
   ngOnInit() {
-    let blog1 = new BlogPost('Blog Post 1','Summary1');
-    this.blogPost.push(blog1);
-    this.blogPost.push(new BlogPost('Blog Post 2','Summary2'));
-    this.blogPost.push(new BlogPost('Blog Post 3','The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component'));
+    // let blog1 = new BlogPost('Blog Post 1','Summary1');
+    // this.blogPost.push(blog1);
+    // this.blogPost.push(new BlogPost('Blog Post 2','Summary2'));
+    // this.blogPost.push(new BlogPost('Blog Post 3','The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component'));
+    this.blogPosts = 
+    [
+      // This is PAGE 1 array
+      [
+        {title: 'Post 1',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'Post 2',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'Post 3',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'Post 4',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'}
+      ],
+      // This is PAGE 2 another array
+      [
+        {title: 'Post 5',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'Post 6',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'Post 7',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'Post 8',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'}
+      ],
+      // This is PAGE 3 array
+      [
+        {title: 'Post 9',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'Post 10',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'Post 11',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'Post 12',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'}
+      ]
+    ];
   }
 
 }
