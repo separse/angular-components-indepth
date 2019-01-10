88bd662f971414058b56a171a5d90ea3ade94bdc
vidhurraj147 <vidhurraj147@gmail.com>
2019-01-10 15:35:18 -0500

    created service and moved useful data to service

--- src/app/blog-data.service.ts
+++ src/app/blog-data.service.ts
@@ -1,9 +1,60 @@
 import { Injectable } from '@angular/core';
+import { BlogPost } from './blog-post';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlogDataService {
 
+
   constructor() { }
+
+  getData(): BlogPost [] []{
+    return [
+      // This is PAGE 1 array
+      [
+        {title: 'PAGE 1 Post 1',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'PAGE 1 Post 2',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'PAGE 1 Post 3',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'PAGE 1 Post 4',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'}
+      ],
+      // This is PAGE 2 another array
+      [
+        {title: 'PAGE 2 Post 5',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'PAGE 2 Post 6',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'PAGE 2 Post 7',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'PAGE 2 Post 8',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'}
+      ],
+      // This is PAGE 3 array
+      [
+        {title: 'Post 9',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'Post 10',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'Post 11',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'Post 12',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'}
+      ],
+      // This is PAGE 4 array
+      [
+        {title: 'PAGE 4 Post 13',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'PAGE 4 Post 14',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'PAGE 4 Post 15',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'},
+        {title: 'PAGE 4 Post 16',
+        summary: 'constructor is recomended use for dependency injection for services etc.,'}
+      ]
+    ];
+  }
 }
--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, ViewEncapsulation, ViewChild, QueryList, ViewChildren } from '@angular/core';
 import { BlogPost } from '../blog-post';
 import { BlogPostTileComponent } from '../blog-post-tile/blog-post-tile.component';
+import { BlogDataService } from '../blog-data.service';
 
 @Component({
   selector: 'app-blog-list',
@@ -16,7 +17,7 @@ export class BlogListComponent implements OnInit {
   // @ViewChild('tile') blogPostTileComponent: BlogPostTileComponent;
   @ViewChildren('tile') blogPostTileComponents: QueryList<BlogPostTileComponent>;
 
-  constructor() { 
+  constructor(private blogdataSerice: BlogDataService) { 
     
   }
 
@@ -30,53 +31,7 @@ export class BlogListComponent implements OnInit {
     // this.blogPost.push(new BlogPost('Blog Post 3','The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component The more compilicated your contructor is MORE difficult to test the class / component'));
     
     this.currentPage = 0;
-    this.blogPosts = 
-    [
-      // This is PAGE 1 array
-      [
-        {title: 'PAGE 1 Post 1',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'},
-        {title: 'PAGE 1 Post 2',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'},
-        {title: 'PAGE 1 Post 3',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'},
-        {title: 'PAGE 1 Post 4',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'}
-      ],
-      // This is PAGE 2 another array
-      [
-        {title: 'PAGE 2 Post 5',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'},
-        {title: 'PAGE 2 Post 6',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'},
-        {title: 'PAGE 2 Post 7',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'},
-        {title: 'PAGE 2 Post 8',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'}
-      ],
-      // This is PAGE 3 array
-      [
-        {title: 'Post 9',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'},
-        {title: 'Post 10',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'},
-        {title: 'Post 11',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'},
-        {title: 'Post 12',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'}
-      ],
-      // This is PAGE 4 array
-      [
-        {title: 'PAGE 4 Post 13',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'},
-        {title: 'PAGE 4 Post 14',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'},
-        {title: 'PAGE 4 Post 15',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'},
-        {title: 'PAGE 4 Post 16',
-        summary: 'constructor is recomended use for dependency injection for services etc.,'}
-      ]
-    ];
+    this.blogPosts = this.blogdataSerice.getData();
   }
 
   updatePage(newPage){
