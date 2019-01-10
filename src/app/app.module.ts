f46b1bf5b63f7ed0a4f8ed35b4c7049e69cff7f4
vidhurraj147 <vidhurraj147@gmail.com>
2019-01-09 23:20:54 -0500

    ng generate component blog-post-tile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlogPostTileComponent } from './blog-post-tile/blog-post-tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlogPostTileComponent
   ],
   imports: [
     BrowserModule,
