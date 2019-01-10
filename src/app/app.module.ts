54e00f3c59f427cbeb7790d9178a7c94c38f13dd
vidhurraj147 <vidhurraj147@gmail.com>
2019-01-09 23:23:36 -0500

    ng g c blog-list _ ng generate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BlogPostTileComponent } from './blog-post-tile/blog-post-tile.component';
+import { BlogListComponent } from './blog-list/blog-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BlogPostTileComponent
+    BlogPostTileComponent,
+    BlogListComponent
   ],
   imports: [
     BrowserModule,
