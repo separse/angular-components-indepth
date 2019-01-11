c2098ec98ae0d299e69482c25227fc6b8b1acd7e
vidhurraj147 <vidhurraj147@gmail.com>
2019-01-10 23:43:29 -0500

    directives implementation

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,23 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
 
-  constructor() { }
+  @Input('appHighlight') color;
+  constructor(private element: ElementRef) {
+    console.log(element);
+    element.nativeElement.style.backgroundColor = 'yellow';
+   }
+
+   @HostListener('mouseenter') addHighlight(){
+    this.element.nativeElement.style.backgroundColor = 'lightblue';
+   }
+
+   @HostListener('mouseleave') removeHighlight(){
+    this.element.nativeElement.style.backgroundColor = this.color;
+   }
+
 
 }
